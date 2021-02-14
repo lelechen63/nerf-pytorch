12b9fa3741e28ca9d2365492526cd6385e47c09a
Lele Chen <lelechen@fb.com>
2021-02-14 17:44:58 -0500

    g

--- llff/poses/pose_utils.py
+++ llff/poses/pose_utils.py
@@ -58,7 +58,7 @@ def save_poses(basedir, poses, pts3d, perm):
     vis_arr = []
     print ('++++++++')
     print (poses.shape)
-    print( pts3d.shape)
+    print( pts3d)
     print( perm.shape)
     for k in pts3d:
         pts_arr.append(pts3d[k].xyz)
