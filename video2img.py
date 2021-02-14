a96bb858fae930d0a4e57903a2cced6bc0473dfb
Lele Chen <lelechen@fb.com>
2021-02-14 17:25:42 -0500

    g

--- video2img.py
+++ video2img.py
@@ -6,13 +6,15 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--v_path', type=str, default='/home/cxu-serve/u1/lchen63/github/nerf-pytorch/data/lele_data/video/IMG_0267.MOV',
                     help='input video path')
-parser.add_argument('--img_path', type=str, default='/home/cxu-serve/u1/lchen63/github/nerf-pytorch/data/lele_data/imgs/IMG_0267',
+parser.add_argument('--v_id', type=str, default='IMG_0267',
+                    help='input video path')
+parser.add_argument('--img_path', type=str, default='/home/cxu-serve/u1/lchen63/github/nerf-pytorch/data/lele_data/images',
                     help='input video path')
 
 args = parser.parse_args()
 
 
-def video2img(v_path = None, img_path = None, step = 10):
+def video2img(v_path = None, v_id = None,  img_path = None, step = 10):
     if not os.path.exists(img_path):
         os.mkdir(img_path)
     raw = cv2.VideoCapture(v_path)
@@ -26,8 +28,8 @@ def video2img(v_path = None, img_path = None, step = 10):
         print(ret,count)
         print(frame.shape)
         if count % step == 0:
-            img_name = os.path.join( img_path, '%05d.png'%count)
+            img_name = os.path.join( img_path, v_id + '_%05d.png'%count)
             cv2.imwrite(img_name, frame)
         count += 1
     return imgs
-video2img(args.v_path, args.img_path)
+video2img(args.v_path, args.img_path, args.v_id)
