09b90789acaa0d88a9d11d44d8b7ecc367e7a24d
Lele Chen <lelechen@fb.com>
2021-02-16 21:50:24 -0500

    g

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -108,11 +108,14 @@ class NeRF(nn.Module):
 
     def forward(self, x):
         print (x.shape,' ++++')
+        
         input_pts, input_views, input_ch_exp = torch.split(x, [self.input_ch, self.input_ch_views, self.input_ch_exp], dim=-1)
         print (input_pts.shape, input_views.shape, input_ch_exp.shape)
         # print (gggg)
         h = torch.cat([input_pts, input_ch_exp], 1)
         for i, l in enumerate(self.pts_linears):
+            print (h.shape)
+            print (self.pts_linears)
             h = self.pts_linears[i](h)
             h = F.relu(h)
             if i in self.skips:
