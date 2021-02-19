680e512529b313660a44c8033b7cae7e1357b926
Lele Chen <lelechen@fb.com>
2021-02-18 21:08:57 -0500

    g

--- video2img.py
+++ video2img.py
@@ -4,24 +4,24 @@ import sys
 import os
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--v_path', type=str, default='/home/cxu-serve/u1/lchen63/github/nerf-pytorch/data/lele_data/video/IMG_0267.MOV',
+parser.add_argument('--v_path', type=str, default='/home/cxu-serve/u1/lchen63/github/nerf-pytorch/data/lele_data/video/IMG_0283.MOV',
                     help='input video path')
-parser.add_argument('--v_id', type=str, default='IMG_0267',
+parser.add_argument('--v_id', type=str, default='IMG_0283',
                     help='input video path')
-parser.add_argument('--img_path', type=str, default='/home/cxu-serve/u1/lchen63/github/nerf-pytorch/data/lele_data/images',
+parser.add_argument('--img_path', type=str, default='/home/cxu-serve/u1/lchen63/github/nerf-pytorch/data/lele_data2/images',
                     help='input video path')
 
 args = parser.parse_args()
 
 
-def video2img(v_path = None, v_id = None,  img_path = None, step = 10):
+def video2img(v_path = None, v_id = None,  img_path = None, step = 50):
     if not os.path.exists(img_path):
         os.mkdir(img_path)
     raw = cv2.VideoCapture(v_path)
     ret = True
     count = 0
     # imgs = []
-    store_step = [0, 65, 70, 75, 80, 85, 90, 95, 100,105, 110, 115, 120, 125, 390,400, 405, 410,420,430]
+    # store_step = [0, 40, 80, 120, 80, 85, 90, 95, 100,105, 110, 115, 120, 125, 390,400, 405, 410,420,430]
     print(len(store_step))
     while ret:
         ret,frame = raw.read()
@@ -29,8 +29,8 @@ def video2img(v_path = None, v_id = None,  img_path = None, step = 10):
             break
         # print(ret,count)
         # print(frame.shape)
-        if count in  store_step:
-        # if count % step == 0:
+        # if count in  store_step:
+        if count % step == 0:
             img_name = os.path.join( img_path, v_id + '_%05d.png'%count)
             cv2.imwrite(img_name, frame)
         count += 1
