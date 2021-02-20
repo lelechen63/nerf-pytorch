3c9b8794d0b7135dc6fd6a4c838069a1f8aa1f2a
Lele Chen <lelechen@fb.com>
2021-02-20 11:35:54 -0500

    g

--- load_llff.py
+++ load_llff.py
@@ -306,7 +306,7 @@ def load_llff_data(basedir, factor=8, recenter=True, bd_factor=.75, spherify=Fal
         # Generate poses for spiral path
         render_poses = render_path_spiral(c2w_path, up, rads, focal, zdelta, zrate=.5, rots=N_rots, N=N_views)
         
-        
+    print ('!!!!!!!!!!', render_poses.shape)
     render_poses = np.array(render_poses).astype(np.float32)
 
     c2w = poses_avg(poses)
