1957d5c25bade52159ca69d67180e0fd67c88086
Lele Chen <lelechen@fb.com>
2021-02-24 17:14:39 -0500

    g

--- run_nerf.py
+++ run_nerf.py
@@ -570,9 +570,9 @@ def config_parser():
                         help='frequency of tensorboard image logging')
     parser.add_argument("--i_weights", type=int, default=10000, 
                         help='frequency of weight ckpt saving')
-    parser.add_argument("--i_testset", type=int, default=5, 
+    parser.add_argument("--i_testset", type=int, default=5000, 
                         help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=5, 
+    parser.add_argument("--i_video",   type=int, default=5000, 
                         help='frequency of render_poses video saving')
 
     return parser
