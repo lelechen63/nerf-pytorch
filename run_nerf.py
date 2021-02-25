df3bf7e661c5d3b5652042f4c50aff4832f08db7
Lele Chen <lelechen@fb.com>
2021-02-25 09:07:01 -0500

    g

--- run_nerf.py
+++ run_nerf.py
@@ -583,7 +583,7 @@ def train():
     parser = config_parser()
     args = parser.parse_args()
     exp_bite = 63
-    args.pretrain = False
+    args.pretrain = True
     args.exp_bite = exp_bite
     # Multi-GPU
     args.n_gpus = torch.cuda.device_count()
